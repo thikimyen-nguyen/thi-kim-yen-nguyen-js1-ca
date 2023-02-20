3414b8bc790f2d3b0a69bbd21eec7c91e75433ab
thikimyen-nguyen <nguyenthikimyen25@gmail.com>
2023-02-20 11:50:32 +0100

    add message error

---
+++ js/components/messages.js
@@ -0,0 +1,3 @@
+function message(type = "sucess", message ="") {
+    return `<div class ="message_${type}">${message}</div>`;
+}
--- js/index.js
+++ js/index.js
@@ -3,16 +3,22 @@ const productsContainer = document.querySelector(".products");
 
 
 async function getMakeup() {
-    const response = await fetch(url);
-    const results = await response.json();
-    console.log(results);
-    results.forEach(function(product) {
-        productsContainer.innerHTML += `<div class="product_card">
-                                        <div class="image"><img src="${product.api_featured_image}" alt="photo of ${product.name}"></div>
-                                        <h3>Price: $${product.price}</h3>
-                                        <p>${product.name}</p>
-                                        </div>`
-    });
+    try {
+        const response = await fetch(url);
+        const results = await response.json();
+        console.log(results);
+        results.forEach(function(product) {
+            productsContainer.innerHTML += `<div class="product_card">
+                                            <div class="image"><img src="${product.api_featured_image}" alt="photo of ${product.name}"></div>
+                                            <h3>Price: $${product.price}</h3>
+                                            <p>${product.name}</p>
+                                            </div>`
+        });
+    } catch (error) {
+        console.log(error);
+        productsContainer.innerHTML = message("error", error);
+    }
+   
    
 }
 getMakeup();
